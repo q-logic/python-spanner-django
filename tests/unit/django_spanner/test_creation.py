8d5f869a748c83f760a8a1818b6a57b43cc7ba47
STATION\MF <mf2199@columbia.edu>
2020-10-28 21:42:36 -0400

    wip: WIP

---
+++ tests/unit/django_spanner/test_creation.py
@@ -0,0 +1,28 @@
+# Copyright 2020 Google LLC
+#
+# Use of this source code is governed by a BSD-style
+# license that can be found in the LICENSE file or at
+# https://developers.google.com/open-source/licenses/bsd
+
+"""Django Spanner DatabaseConnection class unit tests."""
+
+import unittest
+
+from unittest import mock
+
+
+class TestCreation(unittest.TestCase):
+    def test_mark_skips(self):
+        pass
+
+    def test_create_test_db(self):
+        pass
+
+    def test__create_test_db(self):
+        pass
+
+    def test__execute_create_test_db(self):
+        pass
+
+    def test__destroy_test_db(self):
+        pass
